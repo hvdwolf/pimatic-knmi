ea8147cf3810005a6022b35dbdfc2ba21377f3cc
hvdwolf <hvdwolf@gmail.com>
2015-10-12 21:43:20 +0200

    make python 2 and 3 compatible; convert spaces to tabs; add correct shebang to first line

--- weeroverzicht.py
+++ weeroverzicht.py
@@ -1,4 +1,9 @@
+#! /usr/bin/env python
 # -*- coding: utf-8 -*-
+
+# Version 20151012
+# make python3 and python 2 compatible
+
 
 # version 0.1, 20151011, hvdwolf@gmnail.com
 # extremely quick and dirty approach. Needs clean up, improvements, modularisation
@@ -8,7 +13,15 @@
 # ToDo: Try to add weeractueel and full table download from KNMI and xml parse it. Requires currently
 # more effort due to non-DTD characters.
 
-import urllib, urllib2, os
+import urllib, os
+try:
+	# For python3
+	from urllib.request import urlopen
+	python3 = "YES"
+except ImportError:
+	# Use python2
+	from urllib2 import urlopen
+	python3 = "NO"
 
 ### Pimatic settings ###
 # localhost is fine when running from your pimatic server. Otherwise change to suitable ip address or dns. 
@@ -49,43 +62,48 @@ knmi_columns = ['Locatie', 'Weer', 'Temperatuur', 'RV', 'Wind richting', 'Wind (
 # Start the real work
 
 if WeerActueel == "YES":
-        response = urllib2.urlopen(WAURL)
-        html = response.read()
-        split_string = 'Actueel weer ' + WALocation
-        tmppage = html.split(split_string)
-        subpage = str(tmppage[2])
-        #print(subpage)
-        tmppage = subpage.split('<table>')
-        subpage = '<table>' + str(tmppage[1])
-        tmppage = subpage.split('</table>')
-        tabel = str(tmppage[0]) + '</table>'
-        print(tabel)
+	response = urlopen(WAURL)
+	html = response.read()
+	if python3 == "YES":
+		html = str( html, encoding='utf8' )
+	split_string = 'Actueel weer ' + WALocation
+	tmppage = html.split(split_string)
+	subpage = str(tmppage[2])
+	#print(subpage)
+	tmppage = subpage.split('<table>')
+	subpage = '<table>' + str(tmppage[1])
+	tmppage = subpage.split('</table>')
+	tabel = str(tmppage[0]) + '</table>'
+	print(tabel)
 
 if KNMI == 'YES':
-        response = urllib2.urlopen(KNMIURL)
-        html = response.read()
-        tmppage = html.split(KNMILocation + '</td>')
-        subpage = str(tmppage[1])
-        tmppage = subpage.split('</tr>')
-        table = str(tmppage[0])
-        table = table.replace('&nbsp;</td>', '', 99).replace('<td align=right>', '', 99).replace('  ', '', 999).replace('<td>', '', 999).replace('</td>', '', 99)
-        counter = 0
-        for line in table.splitlines():
-                # Finally remove still redundant spaces
-                line = line.replace(' ', '', 99)
-                # Add units to variableDevice in pimatic
-                if knmi_columns[counter] == 'Locatie':
-                        os.system(curl_prefix + KNMILocation + '"}\'  --user "' + pim_user_pass + '" ' + pim_server_url +'knmi-locatie')
-                if knmi_columns[counter] == 'Temperatuur':
-                        os.system(curl_prefix + line + '"}\'  --user "' + pim_user_pass + '" ' + pim_server_url +'knmi-temperatuur')
-                if knmi_columns[counter] == 'RV':
-                        os.system(curl_prefix + line + '"}\'  --user "' + pim_user_pass + '" ' + pim_server_url +'knmi-rv')
-                if knmi_columns[counter] == 'Wind richting':
-                        os.system(curl_prefix + line + '"}\'  --user "' + pim_user_pass + '" ' + pim_server_url +'knmi-wr')
-                if knmi_columns[counter] == 'Wind (m/s)':
-                        os.system(curl_prefix + line + '"}\'  --user "' + pim_user_pass + '" ' + pim_server_url +'knmi-ws')
-                if knmi_columns[counter] == 'Zicht (m)':
-                        os.system(curl_prefix + line + '"}\'  --user "' + pim_user_pass + '" ' + pim_server_url +'knmi-zicht')
-                if knmi_columns[counter] == 'Druk (hPa)':
-                        os.system(curl_prefix + line + '"}\'  --user "' + pim_user_pass + '" ' + pim_server_url +'knmi-druk')
-                counter += 1
+	response = urlopen(KNMIURL)
+	html = response.read()
+	if python3 == "YES":
+		html = str( html, encoding='utf8' )
+	tmppage = html.split(KNMILocation + '</td>')
+	subpage = str(tmppage[1])
+	tmppage = subpage.split('</tr>')
+	table = str(tmppage[0])
+	table = table.replace('&nbsp;</td>', '', 99).replace('<td align=right>', '', 99).replace('  ', '', 999).replace('<td>', '', 999).replace('</td>', '', 99)
+	counter = 0
+	for line in table.splitlines():
+		# Finally remove still redundant spaces
+		line = line.replace(' ', '', 99)
+		# Add units to variableDevice in pimatic
+		if knmi_columns[counter] == 'Locatie':
+			os.system(curl_prefix + KNMILocation + '"}\'  --user "' + pim_user_pass + '" ' + pim_server_url +'knmi-locatie')
+		if knmi_columns[counter] == 'Temperatuur':
+			os.system(curl_prefix + line + '"}\'  --user "' + pim_user_pass + '" ' + pim_server_url +'knmi-temperatuur')
+		if knmi_columns[counter] == 'RV':
+			os.system(curl_prefix + line + '"}\'  --user "' + pim_user_pass + '" ' + pim_server_url +'knmi-rv')
+		if knmi_columns[counter] == 'Wind richting':
+			os.system(curl_prefix + line + '"}\'  --user "' + pim_user_pass + '" ' + pim_server_url +'knmi-wr')
+		if knmi_columns[counter] == 'Wind (m/s)':
+			os.system(curl_prefix + line + '"}\'  --user "' + pim_user_pass + '" ' + pim_server_url +'knmi-ws')
+		if knmi_columns[counter] == 'Zicht (m)':
+			os.system(curl_prefix + line + '"}\'  --user "' + pim_user_pass + '" ' + pim_server_url +'knmi-zicht')
+		if knmi_columns[counter] == 'Druk (hPa)':
+			os.system(curl_prefix + line + '"}\'  --user "' + pim_user_pass + '" ' + pim_server_url +'knmi-druk')
+		counter += 1
+
