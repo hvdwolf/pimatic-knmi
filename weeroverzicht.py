a7b0e70e803407b6dfb0d666fcb5985f476498f8
hvdwolf <hvdwolf@gmail.com>
2015-12-06 15:14:44 +0100

    update of script to github not entirely succesful

--- weeroverzicht.py
+++ weeroverzicht.py
@@ -1,13 +1,13 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# Version 20151012
+# Version 20151206
 # make python3 and python 2 compatible
 
+# Version 0.2, 20151206, Full rewrite of script. Also add new Chill variable
 # version 0.1, 20151011, hvdwolf@gmnail.com
 # extremely quick and dirty approach. Needs clean up, improvements, modularisation
 # Currently uses OS curl to post. Needs to be python posting via urllib or pycurl
-# Currently only python 2
 
 # ToDo: Try to add weeractueel and full table download from KNMI and xml parse it. Requires currently
 # more effort due to non-DTD characters.
@@ -30,17 +30,8 @@ except ImportError:
 # Can be https as well. Also: if you use another port then 80 or 443 do not forget to add the port number.
 pimatic_server = 'localhost'
 # user and password. I prefer a special posting user having a role of varposter with only  "variables": "write" in the varposter role. Rest to "none" or false.
-pimatic_user ='apiposter'
-pimatic_pass = 'ap1p0ster'
-
-### Weer actueel ###
-# Has much better spreading. Check update sequence from relevant page
-# http://www.hetweeractueel.nl/
-# Is not really free
-WeerActueel = "NO" # use in script: YES or NO
-WALocation = "Zwolle"
-WAURL = 'http://www.hetweeractueel.nl/weer/' + WALocation + '/actueel/'
-
+pimatic_user ='username'
+pimatic_pass = 'password'
 
 ### KNMI ###
 # central public FTP page ftp://ftp.knmi.nl/pub_weerberichten/tabel_10min_data.html
@@ -59,9 +50,6 @@ pim_user_pass = pimatic_user + ':' + pimatic_pass
 curl_prefix = 'curl --silent --insecure -X PATCH --header "Content-Type:application/json" --data \'{"type": "value", "valueOrExpression": "'
 pim_server_url = pimatic_server + '/api/variables/'
 KNMIURL = 'ftp://ftp.knmi.nl/pub_weerberichten/tabel_10min_data.html'
-# Define an identifying column list. Add some empty entries due to possible trailing rubbish
-#knmi_columns = ['Locatie', 'Weer', 'Temperatuur', 'Chill', 'RV', 'Wind richting', 'Wind (m/s)', 'Zicht (m)', 'Druk (hPa)', '', '', '' ]
-
 
 # Start the real work
 if KNMI == 'YES':
